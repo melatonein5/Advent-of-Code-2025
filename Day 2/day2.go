2124ccbdf211d1ea84ba8eb2a8bbe82032d40591
Joseph McCallum-Nattrass <jmn@jmnetwork.uk>
2025-12-04 14:58:51 +0000

    Put thoughts down on day 2 part 1. I am taking a notepad down the pub, I think I can make this work really well

---
+++ Day 2/day2.go
@@ -0,0 +1,45 @@
+package main
+
+/*--- Day 2: Gift Shop ---
+
+You get inside and take the elevator to its only other stop: the gift shop. "Thank you for visiting the North Pole!" gleefully exclaims a nearby sign. You aren't sure who is even allowed to visit the North Pole, but you know you can access the lobby through here, and from there you can access the rest of the North Pole base.
+
+As you make your way through the surprisingly extensive selection, one of the clerks recognizes you and asks for your help.
+
+As it turns out, one of the younger Elves was playing on a gift shop computer and managed to add a whole bunch of invalid product IDs to their gift shop database! Surely, it would be no trouble for you to identify the invalid product IDs for them, right?
+
+They've even checked most of the product ID ranges already; they only have a few product ID ranges (your puzzle input) that you'll need to check. For example:
+
+11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124
+
+(The ID ranges are wrapped here for legibility; in your input, they appear on a single long line.)
+
+The ranges are separated by commas (,); each range gives its first ID and last ID separated by a dash (-).
+
+Since the young Elf was just doing silly patterns, you can find the invalid IDs by looking for any ID which is made only of some sequence of digits repeated twice. So, 55 (5 twice), 6464 (64 twice), and 123123 (123 twice) would all be invalid IDs.
+
+None of the numbers have leading zeroes; 0101 isn't an ID at all. (101 is a valid ID that you would ignore.)
+
+Your job is to find all of the invalid IDs that appear in the given ranges. In the above example:
+
+    11-22 has two invalid IDs, 11 and 22.
+    95-115 has one invalid ID, 99.
+    998-1012 has one invalid ID, 1010.
+    1188511880-1188511890 has one invalid ID, 1188511885.
+    222220-222224 has one invalid ID, 222222.
+    1698522-1698528 contains no invalid IDs.
+    446443-446449 has one invalid ID, 446446.
+    38593856-38593862 has one invalid ID, 38593859.
+    The rest of the ranges contain no invalid IDs.
+
+Adding up all the invalid IDs in this example produces 1227775554.
+
+What do you get if you add up all of the invalid IDs?
+*/
+
+//Firstly, we don't need to solve for odd length IDs, as they cannot be made of a sequence repeated twice. Therefore, we can skip any ranges that are entirely odd-length.
+//Secondly, we can break down the ranges into sub-ranges of even-length numbers. For example, the range 950-1150 can be broken down into 950-999 and 1000-1150. This means we only need to check numbers of the same length against each other.
+//Finally, for a given range, we don't need to iterate, for 2 digit numbers, we know that mirrors of every 1 digit number will be invalid. For 4 digit numbers, we know that mirrors of every 2 digit number will be invalid, and so on. Therefore, we can generate the invalid numbers directly rather than iterating through the entire range.
+//I think I am going to use a recursive function to generate the mirrors of numbers of a given length.
